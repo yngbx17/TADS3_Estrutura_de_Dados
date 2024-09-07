813b533ee2f1ef3ab47f1850bcfe02daaa0cfe5a
Diego de Souza <142831006+yngbx17@users.noreply.github.com>
2024-09-06 21:25:39 -0300

    Aula 4
    Filas, pilhas e vetores com objetos

---
+++ Aula5/Pilha.java
@@ -0,0 +1,31 @@
+package Aula5;
+
+public class Pilha <T> extends EstruturaEstatica<T> {
+    public Pilha(){
+        super();
+
+    }
+    public Pilha(int capacidade){
+        super(capacidade);
+    }
+    public void empilha(T elemento){
+        super.adiciona(elemento);
+    }
+
+    public T topo() {
+        if (this.estaVazia()) {
+            return null;
+        } else {
+            return this.elementos[tamanho - 1];
+        }
+    }
+        public T desempilha(){
+            if(this.estaVazia()){
+                return null;
+            }
+            T elemento = this.elementos[tamanho-1];
+            tamanho--;
+            return elemento;
+        }
+    }
+
